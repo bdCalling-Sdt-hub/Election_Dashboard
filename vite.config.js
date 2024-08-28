eb14e765da6923ddd4e5c42a269987ddf0b1c240
Mithila <mithilakhan082@gmail.com>
2024-08-28 12:21:15 +0600

    add codes

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "192.168.10.211",
-    port: 3002,
+    host: "143.244.131.216",
+    port: 3001,
   },
 }); 
 
