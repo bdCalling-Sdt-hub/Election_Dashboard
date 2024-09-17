8fc0c0f2f0793d3a2e8d51e920b9a39157ea2865
Nadir Hossain <nadirhossain336@gmail.com>
2024-09-17 18:38:40 +0600

    :recycle: refactor

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    // host: "142.93.201.200",
-    host: "192.168.10.195",
+    host: "142.93.201.200",
+    // host: "192.168.10.195",
     port: 3001,
   },
 }); 
